cae2e24b707aeaa9f043c956ab6c6d77d9c67a55
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-06-05 00:45:56 +0530

    Time: 0 ms (100%), Space: 7.9 MB (57.05%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,18 +2,27 @@ class Solution {
 public:
         int dp[46];
     
-    Solution() {
-        std::fill(dp, dp + 46, -1); 
-    }
+    // Solution() {
+    //     std::fill(dp, dp + 46, -1); 
+    // }
 
-    int climbStairs(int n) {
-         if(n<0)
-         return 0;
-         else if(n==0)
-         return 1;
-         else if(dp[n]!=-1)
-         return dp[n];
-         else
-         return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+//     int climbStairs(int n) {
+//          if(n<0)
+//          return 0;
+//          else if(n==0)
+//          return 1;
+//          else if(dp[n]!=-1)
+//          return dp[n];
+//          else
+//          return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+//     }
+// };
+int climbStairs(int n) {
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
     }
-};
+    return dp[n];
+}};
+
