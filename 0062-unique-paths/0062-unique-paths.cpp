0a943a1bb5b5169f7908a3b8d07790a0a3110bb9
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-06-11 02:16:25 +0530

    Time: 0 ms (100%), Space: 8.3 MB (79.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -12,22 +12,28 @@ int solve(int i,int j,vector<vector<int>>&dp){
     return dp[i][j]=left+up;
 }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        dp[0][0]=1;
+        // vector<vector<int>>dp(m,vector<int>(n,-1));
+        // dp[0][0]=1;
     //    return solve(m-1,n-1,dp);
+    vector<int>prev(n,0);
        for(int i=0;i<m;i++){
+        vector<int>temp(n,0);
         for(int j=0;j<n;j++){
             if(i==0 && j==0)
-            continue;
+            temp[j]=1;
+            else
+            {
              int left=0;
              int up=0;
-             if(i>=1)
-             up=dp[i-1][j];
+            //  if(i>=1)
+             up=prev[j];
              if(j>=1)
-             left=dp[i][j-1];
-             dp[i][j]=left+up;
+             left=temp[j-1];
+             temp[j]=left+up;
+            }
         }
+        prev=temp;
        }
-       return dp[m-1][n-1];
+       return prev[n-1];
     }
 };
