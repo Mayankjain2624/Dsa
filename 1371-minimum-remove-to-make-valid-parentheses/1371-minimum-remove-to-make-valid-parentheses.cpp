ac7d8ac2e8cef8670013cdd88aec5433350572e9
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-06-26 04:03:28 +0530

    Time: 19 ms (10.29%), Space: 13.4 MB (86.05%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<pair<char,int>>st;
+        // stack<pair<char,int>>st;
+        stack<int>st;
         for(int i=0;i<s.length();i++){
             char ch=s[i];
             if(ch=='(')
-            st.push({ch,i});
+            st.push(i);
             else if(ch==')'){
-                if(!st.empty() && st.top().first=='(')
+                if(!st.empty() && s[st.top()]=='(')
                 st.pop();
                 else
-                st.push({ch,i});
+                st.push(i);
             }
             else
             continue;
         }
         while(!st.empty()){
-            s.erase(st.top().second,1);
+            s.erase(st.top(),1);
             // cout<<st.top().first<<" "<<st.top().second<<" ";
             st.pop();
         }
