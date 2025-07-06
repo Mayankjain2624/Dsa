a0a497763c4984c7ae8330b7d9d0a691f5f527ff
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-06 15:03:22 +0530

    Time: 0 ms (100%), Space: 52.2 MB (63.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int i,vector<int>&dp){
-        if(i>=nums.size()-1)
-            return true;
-        if(nums[i]==0)
-        return false;
-        if(dp[i]!=-1)
-            return dp[i];
-        bool ans=false;
-        for(int j=1;j<=nums[i];j++){
-            if(ans==true)
-            break;
-            if(i+j<nums.size())
-            ans=ans || solve(nums,i+j,dp);
-        }
-        return dp[i]=ans;
-    }
+    // int solve(vector<int>&nums,int i,vector<int>&dp){
+    //     if(i>=nums.size()-1)
+    //         return true;
+    //     if(nums[i]==0)
+    //     return false;
+    //     if(dp[i]!=-1)
+    //         return dp[i];
+    //     bool ans=false;
+    //     for(int j=1;j<=nums[i];j++){
+    //         if(ans==true)
+    //         break;
+    //         if(i+j<nums.size())
+    //         ans=ans || solve(nums,i+j,dp);
+    //     }
+    //     return dp[i]=ans;
+    // }
     bool canJump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return solve(nums,0,dp);
+        // vector<int>dp(nums.size(),-1);
+        // return solve(nums,0,dp);
+        int r=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i>r)
+            return false;
+            else
+            {
+                int k=nums[i]+i;
+                r=max(r,k);
+            }
+        }
+        return true;
     }
 };
