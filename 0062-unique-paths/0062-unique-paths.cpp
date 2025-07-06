58f7b3d225738eedd1c768061ad8fee50fec7fef
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-06 15:57:47 +0530

    Time: 3 ms (8.45%), Space: 9.4 MB (8.8%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,39 +1,13 @@
 class Solution {
 public:
-int solve(int i,int j,vector<vector<int>>&dp){
-    if(i==0 &&j==0)
-    return 1;
-    if(i<0 || j<0)
-    return 0;
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-    int left=solve(i,j-1,dp);
-    int up=solve(i-1,j,dp);
-    return dp[i][j]=left+up;
-}
     int uniquePaths(int m, int n) {
-        // vector<vector<int>>dp(m,vector<int>(n,-1));
-        // dp[0][0]=1;
-    //    return solve(m-1,n-1,dp);
-    vector<int>prev(n,0);
-       for(int i=0;i<m;i++){
-        vector<int>temp(n,0);
-        for(int j=0;j<n;j++){
-            if(i==0 && j==0)
-            temp[j]=1;
-            else
-            {
-             int left=0;
-             int up=0;
-            //  if(i>=1)
-             up=prev[j];
-             if(j>=1)
-             left=temp[j-1];
-             temp[j]=left+up;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        dp[1][1]=1;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j]+=(dp[i-1][j]+dp[i][j-1]);
             }
         }
-        prev=temp;
-       }
-       return prev[n-1];
+        return dp[m][n];
     }
 };
