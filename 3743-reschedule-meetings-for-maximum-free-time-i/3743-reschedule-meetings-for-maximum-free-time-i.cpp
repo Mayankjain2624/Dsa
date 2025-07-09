8f8c2677b8b54061dc6ab88ab1ce2ee766051fe1
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-10 01:17:48 +0530

    Time: 0 ms (100%), Space: 125.9 MB (19.47%) - LeetHub

--- 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/3743-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
         int n=startTime.size();
-        vector<int>free(n+1);
-        free[0]=startTime[0]-0;
-        free[n]=eventTime-endTime[n-1];
+        vector<int>free;
+        free.push_back(startTime[0]);
         for(int i=1;i<n;i++){
-           free[i]=startTime[i]-endTime[i-1];
+           free.push_back(startTime[i]-endTime[i-1]);
         }
+        free.push_back(eventTime-endTime[n-1]);
             int l=0;
             int r=min(k+1,n+1);
             int currentSum=0;
@@ -16,11 +16,8 @@ public:
             }
             int maxi=currentSum;
             while(r<=n){
-                currentSum-=free[l];
-                currentSum+=free[r];
+                currentSum+=(free[r++]-free[l++]);
                 maxi=max(maxi,currentSum);
-                r++;
-                l++;
             }
             return maxi;
     }
