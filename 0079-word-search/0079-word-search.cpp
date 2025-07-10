f2458056362f3d61c713d9ba404ce1e2127506fc
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-10 17:16:12 +0530

    Time: 239 ms (69.25%), Space: 10.4 MB (86.9%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,30 +1,32 @@
 class Solution {
+        const vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 public:
-    bool dfs(vector<vector<char>>& board,int i,int j,string &word,int l){
+    bool dfs(vector<vector<char>>& board,int i,int j,const string &word,int l){
         if(l==word.size())
         return true;
         if(i<0 || j<0 || i>=board.size() || j>=board[0].size())
         return false;
         if(board[i][j]!=word[l])
         return false;
-
         char ch=board[i][j];
         board[i][j]='#';
-         vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
          for(auto &it:dirs){
             int ni=i+it.first;
             int nj=j+it.second;
-            if(dfs(board,ni,nj,word,l+1))
-            return true;
+        //    if (ni >= 0 && nj >= 0 && ni < board.size() && nj < board[0].size() &&
+        //     board[ni][nj] == word[l + 1]) {
+            if (dfs(board, ni, nj, word, l + 1))
+                return true;
+        //  }
          }
-        board[i][j]=ch;
+         board[i][j]=ch;
         return false;
     }
-    bool exist(vector<vector<char>>& board, string word) {
+    bool exist(vector<vector<char>>& board,const string& word) {
          int m = board.size(), n = board[0].size();
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (dfs(board, i, j, word, 0))
+                if (board[i][j] == word[0] && dfs(board, i, j, word, 0))
                     return true;
             }
         }
