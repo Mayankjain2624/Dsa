5daa5eb96dffadd0f50a85f03ed52c0f9f5a461b
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-17 15:47:11 +0530

    Time: 181 ms (86.69%), Space: 196.7 MB (54.52%) - LeetHub

--- 2321-minimum-weighted-subgraph-with-the-required-paths/2321-minimum-weighted-subgraph-with-the-required-paths.cpp
+++ 2321-minimum-weighted-subgraph-with-the-required-paths/2321-minimum-weighted-subgraph-with-the-required-paths.cpp
@@ -1,51 +1,52 @@
 class Solution {
 public:
-    vector<long long> dijstra(long long  src, vector<vector<pair<long long , long long >>>& adj) {
-        priority_queue<pair<long long, long long >, vector<pair<long long, long long >>,
-                       greater<>>
-            pq;
-        vector<long long> dist(adj.size(), LLONG_MAX);
-        dist[src]=0;
+    using ll = long long;
+    using pii = pair<ll, ll>;
+
+    vector<ll> dijkstra(int src, const vector<vector<pii>>& adj) {
+        int n = adj.size();
+        vector<ll> dist(n, LLONG_MAX);
+        priority_queue<pii, vector<pii>, greater<>> pq;
+
+        dist[src] = 0;
         pq.push({0, src});
+
         while (!pq.empty()) {
-            auto [cost, node] = pq.top();
+            auto [cost, u] = pq.top();
             pq.pop();
-            if (cost > dist[node])
-                continue;
-            for (auto& it : adj[node]) {
-                long long  nc = cost + it.second;
-                long long  adjnode = it.first;
-                if (dist[adjnode] > nc) {
-                    dist[adjnode] = nc;
-                    pq.push({nc, adjnode});
+
+            if (cost > dist[u]) continue;
+
+            for (const auto& [v, w] : adj[u]) {
+                if (dist[v] > cost + w) {
+                    dist[v] = cost + w;
+                    pq.push({dist[v], v});
                 }
             }
         }
         return dist;
     }
-    long long minimumWeight(int n, vector<vector<int>>& edges, int  src1,
-                            int  src2, int  dest) {
-        vector<vector<pair<long long , long long >>> adj(n), radj(n);
-        for (auto& it : edges) {
-            long long  u = it[0];
-            long long  v = it[1];
-            long long  w = it[2];
-            adj[u].push_back({v, w});
-            radj[v].push_back({u, w});
+
+    ll minimumWeight(int n, vector<vector<int>>& edges, int src1, int src2, int dest) {
+        vector<vector<pii>> adj(n), radj(n);
+
+        for (const auto& e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            adj[u].emplace_back(v, w);
+            radj[v].emplace_back(u, w); 
         }
-        vector<long long> ds1 = dijstra(src1, adj);
-        vector<long long> ds2 = dijstra(src2, adj);
-        vector<long long> dfd = dijstra(dest, radj);
 
-        long long minweight = LLONG_MAX;
-for (long long i = 0; i < n; i++) {
-    if (ds1[i] != LLONG_MAX && ds2[i] != LLONG_MAX && dfd[i] != LLONG_MAX) {
-        minweight = min(minweight, ds1[i] + ds2[i] + dfd[i]);
-    }
-}
+        vector<ll> d1 = dijkstra(src1, adj);
+        vector<ll> d2 = dijkstra(src2, adj);
+        vector<ll> dd = dijkstra(dest, radj);
 
-        if (minweight == LLONG_MAX)
-            return -1;
-        return minweight;
+        ll res = LLONG_MAX;
+        for (int i = 0; i < n; ++i) {
+            if (d1[i] == LLONG_MAX || d2[i] == LLONG_MAX || dd[i] == LLONG_MAX) 
+            continue;
+            res = min(res, d1[i] + d2[i] + dd[i]);
+        }
+
+        return res == LLONG_MAX ? -1 : res;
     }
 };
