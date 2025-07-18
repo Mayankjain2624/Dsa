876569390cc2185fdba580e543198c410a8db97c
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-18 14:49:43 +0530

    Time: 228 ms (67.03%), Space: 108.1 MB (95.49%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,55 +1,40 @@
 class Solution {
 public:
-vector<int>dx={1,-1,0,0};
-vector<int>dy={0,0,1,-1};
-bool isvalid(int x,int y,int n,int m){
-    if(x>=0 && x<n && y>=0 && y<m)
-    return true;
-    else
-    return false;
-}
+    vector<int> dx = {1, -1, 0, 0};
+    vector<int> dy = {0, 0, 1, -1};
+    bool isvalid(int x, int y, int n, int m) {
+        if (x >= 0 && x < n && y >= 0 && y < m)
+            return true;
+        return false;
+    }
     int minimumObstacles(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        if(grid[0][0]==0)
-        pq.push({0,0});
-        else
-        pq.push({1,0});
-        vector<vector<int>>dist(n,vector<int>(m,1e9));
-        // vector<vector<int>>vis(n,vector<int>(m,1e9));
-        dist[0][0]=grid[0][0]? 1 : 0;
-        while(!pq.empty()){
-            auto top=pq.top();
-            int cost=top.first;
-            int x=top.second/m;
-            int y=top.second%m;
+        int n = grid.size();
+        int m = grid[0].size();
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+        pq.push({grid[0][0], 0});
+        dist[0][0] = grid[0][0] ? 1 : 0;
+        while (!pq.empty()) {
+            auto top = pq.top();
+            int cost = top.first;
+            int x = top.second / m;
+            int y = top.second % m;
             pq.pop();
-            if(x==n-1 && y==m-1)
-            return cost;
-            // if(vis[x][y])
-            // continue;
-            // vis[x][y]=1;
-            for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                if(isvalid(nx,ny,n,m)){
-                     if(grid[nx][ny]==1)
-                     {
-                        if(dist[nx][ny]>cost+1)
-                        {
-                            dist[nx][ny]=cost+1;
-                        pq.push({cost+1,nx*m+ny});
-                        }
-                     }
-                     else
-                     {
-                        if(dist[nx][ny]>cost)
-                        {
-                            dist[nx][ny]=cost;
-                     pq.push({cost,nx*m+ny});
-                        }
-                     }
+            if (x == n - 1 && y == m - 1)
+                return cost;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (isvalid(nx, ny, n, m) && grid[nx][ny] == 1 &&
+                    dist[nx][ny] > cost + 1) {
+                    dist[nx][ny] = cost + 1;
+                    pq.push({cost + 1, nx * m + ny});
+                } else if (isvalid(nx, ny, n, m) && grid[nx][ny] == 0 &&
+                           dist[nx][ny] > cost) {
+                    dist[nx][ny] = cost;
+                    pq.push({cost, nx * m + ny});
                 }
             }
         }
