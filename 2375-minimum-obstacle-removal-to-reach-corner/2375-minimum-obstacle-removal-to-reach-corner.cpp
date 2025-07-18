6ec7672c4c0bccf7c988ef658db89b7146543695
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-18 14:32:30 +0530

    Time: 451 ms (29.8%), Space: 110.2 MB (61.52%) - LeetHub

---
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+vector<int>dx={1,-1,0,0};
+vector<int>dy={0,0,1,-1};
+bool isvalid(int x,int y,int n,int m){
+    if(x>=0 && x<n && y>=0 && y<m)
+    return true;
+    else
+    return false;
+}
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        if(grid[0][0]==0)
+        pq.push({0,{0,0}});
+        else
+        pq.push({1,{0,0}});
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        while(!pq.empty()){
+            auto top=pq.top();
+            int cost=top.first;
+            int x=top.second.first;
+            int y=top.second.second;
+            pq.pop();
+            if(x==n-1 && y==m-1)
+            return cost;
+            if(vis[x][y])
+            continue;
+            vis[x][y]=1;
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(isvalid(nx,ny,n,m) && !vis[nx][ny]){
+                     if(grid[nx][ny]==1)
+                     pq.push({cost+1,{nx,ny}});
+                     else
+                     pq.push({cost,{nx,ny}});
+                }
+            }
+        }
+        return -1;
+    }
+};
