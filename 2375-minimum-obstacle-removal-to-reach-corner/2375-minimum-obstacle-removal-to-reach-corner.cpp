cb0f05c2e4dd56dc3f4335d471fb214d34a8bd3e
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-18 14:51:35 +0530

    Time: 131 ms (77.55%), Space: 121.8 MB (42.65%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -2,42 +2,40 @@ class Solution {
 public:
     vector<int> dx = {1, -1, 0, 0};
     vector<int> dy = {0, 0, 1, -1};
+
     bool isvalid(int x, int y, int n, int m) {
-        if (x >= 0 && x < n && y >= 0 && y < m)
-            return true;
-        return false;
+        return x >= 0 && x < n && y >= 0 && y < m;
     }
+
     int minimumObstacles(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            pq;
+        int n = grid.size(), m = grid[0].size();
+        deque<pair<int, int>> dq;
         vector<vector<int>> dist(n, vector<int>(m, 1e9));
-        pq.push({grid[0][0], 0});
-        dist[0][0] = grid[0][0] ? 1 : 0;
-        while (!pq.empty()) {
-            auto top = pq.top();
-            int cost = top.first;
-            int x = top.second / m;
-            int y = top.second % m;
-            pq.pop();
-            if (x == n - 1 && y == m - 1)
-                return cost;
+
+        dq.push_front({0, 0});
+        dist[0][0] = 0;
+
+        while (!dq.empty()) {
+            auto [x, y] = dq.front();
+            dq.pop_front();
+
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (isvalid(nx, ny, n, m) && grid[nx][ny] == 1 &&
-                    dist[nx][ny] > cost + 1) {
-                    dist[nx][ny] = cost + 1;
-                    pq.push({cost + 1, nx * m + ny});
-                } else if (isvalid(nx, ny, n, m) && grid[nx][ny] == 0 &&
-                           dist[nx][ny] > cost) {
-                    dist[nx][ny] = cost;
-                    pq.push({cost, nx * m + ny});
+
+                if (isvalid(nx, ny, n, m)) {
+                    int newCost = dist[x][y] + grid[nx][ny];
+                    if (newCost < dist[nx][ny]) {
+                        dist[nx][ny] = newCost;
+                        if (grid[nx][ny] == 0)
+                            dq.push_front({nx, ny});
+                        else
+                            dq.push_back({nx, ny});
+                    }
                 }
             }
         }
-        return -1;
+
+        return dist[n-1][m-1];
     }
 };
