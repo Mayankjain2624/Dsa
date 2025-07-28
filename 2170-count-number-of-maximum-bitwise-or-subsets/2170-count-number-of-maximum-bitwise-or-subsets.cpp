8f5620980835fb65db0e963ecb8c30af9b0239c7
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-28 19:39:17 +0530

    Time: 126 ms (16.06%), Space: 310.3 MB (9.66%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -3,7 +3,7 @@ public:
     int maxp = 0;
     unordered_map<int, unordered_map<int, int>> dp;
 
-    int f(vector<int>& nums, int i, int x) {
+    int f(vector<int>& nums, int i, int x,vector<vector<int>>&dp) {
         if (i == nums.size())
         {
             if(x==maxp)
@@ -11,16 +11,17 @@ public:
             else
             return 0;
         }
-        if(dp[i].count(x)>0)
+        if(dp[i][x]!=-1)
         return dp[i][x];
-           int pick = f(nums, i + 1, x | nums[i]);
-           int notpick = f(nums, i + 1, x);
+           int pick = f(nums, i + 1, x | nums[i],dp);
+           int notpick = f(nums, i + 1, x,dp);
         return dp[i][x]=pick + notpick;
     }
     int countMaxOrSubsets(vector<int>& nums) {
         for (int i : nums) {
             maxp = maxp | i;
         }
-        return f(nums, 0, 0);
+        vector<vector<int>>dp(nums.size(),vector<int>(maxp+1,-1));
+        return f(nums, 0, 0,dp);
     }
 };
