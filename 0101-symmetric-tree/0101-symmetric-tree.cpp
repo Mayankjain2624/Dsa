3d2e40f04821998098836ed5c3fecfed1b9d3343
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-29 04:04:23 +0530

    Time: 0 ms (100%), Space: 18.9 MB (5.91%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -6,39 +6,38 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         // vector<vector<int>>ans;
-            vector<int>level;
-        if(root==nullptr)
-        return true;
-        queue<TreeNode*>q;
+        vector<int> level, lc;
+        if (root == nullptr)
+            return true;
+        queue<TreeNode*> q;
         q.push(root);
-        while(!q.empty()){
-            int s=q.size();
-            while(s--){
-            TreeNode* node=q.front();
-            q.pop();
-            if(node==nullptr)
-            level.push_back(INT_MIN);
-            // if(node->left!=nullptr)
-            else{
-            q.push(node->left);
-            // if(node->right!=nullptr)
-            q.push(node->right);
-            level.push_back(node->val);
+        while (!q.empty()) {
+            int s = q.size();
+            while (s--) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node == nullptr)
+                    level.push_back(INT_MIN);
+                else {
+                    q.push(node->left);
+                    q.push(node->right);
+                    level.push_back(node->val);
+                }
             }
-            }
-            // ans.push_back(level);
-            vector<int>lc=level;
-            reverse(lc.begin(),lc.end());
-            if(lc!=level)
-            return false;
+            lc = level;
+            reverse(lc.begin(), lc.end());
+            if (lc != level)
+                return false;
             level.clear();
+            lc.clear();
         }
         return true;
     }
