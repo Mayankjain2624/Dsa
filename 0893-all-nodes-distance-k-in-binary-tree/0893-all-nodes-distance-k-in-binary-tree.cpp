b30bbfe88178add90fb73238bfe97164f04590c2
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-29 23:25:18 +0530

    Time: 8 ms (23.87%), Space: 15.9 MB (20.68%) - LeetHub

---
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+vector<int>ans;
+    void solve(int x,int d,vector<vector<int>>&adj,int k,vector<int>&vis){
+       if(vis[x])
+       return;
+       vis[x]=1;
+       if(d==k)
+       {
+        ans.push_back(x);
+        return;
+       }
+       for(int i:adj[x]){
+        solve(i,d+1,adj,k,vis);
+       }
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<vector<int>>adj(500);
+        queue<TreeNode*>q;
+        if(root!=nullptr)
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* node=q.front();
+            q.pop();
+            if(node->left!=nullptr)
+            {
+            adj[node->val].push_back(node->left->val);
+            adj[node->left->val].push_back(node->val);
+            q.push(node->left);
+            }
+            if(node->right!=nullptr)
+            {
+            adj[node->val].push_back(node->right->val);
+            adj[node->right->val].push_back(node->val);
+            q.push(node->right);
+            }
+        }
+        vector<int>vis(500);
+        for(auto &it:adj){
+            for(int i:it){
+                cout<<i<<" ";
+            }
+            cout<<endl;
+        }
+        // [target->val]=1;
+       solve(target->val,0,adj,k,vis);
+    //    cout<<ans<<endl;
+       return ans;
+    }
+};
