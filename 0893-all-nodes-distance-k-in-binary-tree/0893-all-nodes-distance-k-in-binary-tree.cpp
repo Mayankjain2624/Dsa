286457832697cdf566fe4944eb6f9cce1d841f8c
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-07-29 23:42:16 +0530

    Time: 7 ms (29.43%), Space: 15.7 MB (25.14%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,43 +9,61 @@
  */
 class Solution {
 public:
-vector<int>ans;
-    void solve(int x,int d,unordered_map<int,vector<int>>&adj,int k,unordered_map<int,bool>&vis){
-       if(vis[x])
-       return;
-       vis[x]=true;
-       if(d==k)
-       {
-        ans.push_back(x);
-        return;
-       }
-       for(int i:adj[x]){
-        solve(i,d+1,adj,k,vis);
-       }
-    }
+    // void solve(int x,int d,unordered_map<int,vector<int>>&adj,int
+    // k,unordered_map<int,bool>&vis){
+    //    if(vis[x])
+    //    return;
+    //    vis[x]=true;
+    //    if(d==k)
+    //    {
+    //     ans.push_back(x);
+    //     return;
+    //    }
+    //    for(int i:adj[x]){
+    //     solve(i,d+1,adj,k,vis);
+    //    }
+    // }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<int,vector<int>>adj;
-        queue<TreeNode*>q;
-        if(root!=nullptr)
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* node=q.front();
+        unordered_map<int, vector<int>> adj;
+        queue<TreeNode*> q;
+        vector<int> ans;
+        if (root != nullptr)
+            q.push(root);
+        while (!q.empty()) {
+            TreeNode* node = q.front();
             q.pop();
-            if(node->left!=nullptr)
-            {
-            adj[node->val].push_back(node->left->val);
-            adj[node->left->val].push_back(node->val);
-            q.push(node->left);
+            if (node->left != nullptr) {
+                adj[node->val].push_back(node->left->val);
+                adj[node->left->val].push_back(node->val);
+                q.push(node->left);
             }
-            if(node->right!=nullptr)
-            {
-            adj[node->val].push_back(node->right->val);
-            adj[node->right->val].push_back(node->val);
-            q.push(node->right);
+            if (node->right != nullptr) {
+                adj[node->val].push_back(node->right->val);
+                adj[node->right->val].push_back(node->val);
+                q.push(node->right);
             }
         }
-        unordered_map<int,bool>vis;
-       solve(target->val,0,adj,k,vis);
-       return ans;
+        unordered_set<int> vis;
+        queue<pair<int, int>> q1;
+        q1.push({target->val, 0});
+        vis.insert(target->val);
+
+        while (!q1.empty()) {
+            int node = q1.front().first;
+            int d = q1.front().second;
+            q1.pop();
+            if (d == k) {
+                ans.push_back(node);
+                continue;
+            }
+            for (int i : adj[node]) {
+                if (!vis.count(i)) {
+                    vis.insert(i);
+                    q1.push({i, d + 1});
+                }
+            }
+        }
+
+        return ans;
     }
 };
