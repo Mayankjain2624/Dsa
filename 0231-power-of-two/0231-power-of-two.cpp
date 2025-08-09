3c690432b077d85ee32132384e9d2f75d8bf4767
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-08-09 12:02:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.81%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,10 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
         if(n<=0)
         return false;
-        if(n==0)
-        return false;
-            int x=log2(abs(n));
-        if(1<<x==abs(n))
+            int x=log2(n);
+        if(1<<x==n)
         return true;
         return false;
         
