5e3e7aa49d51b268b7b60005f5c3ac3c38dfa7b6
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-08-25 02:53:16 +0530

    Time: 3 ms (79.95%), Space: 18.9 MB (96.06%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& edges) {
+        vector<int>adj[V];
+        vector<int>dis(V,INT_MAX);
+        vector<int>indegree(V);
+        for(auto &it:edges){
+            int u=it[0];
+            int v=it[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        queue<int>q;
+        vector<int>topo;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0)
+            q.push(i);
+        }
+        while(!q.empty()){
+            int node=q.front();
+            topo.push_back(node);
+            q.pop();
+            for(auto &pr:adj[node]){
+                indegree[pr]--;
+                if(indegree[pr]==0)
+                q.push(pr);
+            }
+        }
+        if(topo.size()==V)
+        return true;
+        return false;
+    }
+};
