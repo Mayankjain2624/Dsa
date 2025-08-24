d062fc877fdcb2b66a3e0cd34cf3f09d45b96765
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-08-25 03:02:38 +0530

    Time: 11 ms (11.1%), Space: 18.2 MB (60.23%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& edges) {
+       vector<vector<int>> adj(V);
+        vector<int>dis(V,INT_MAX);
+        vector<int>indegree(V);
+        for(auto &it:edges){
+            int u=it[0];
+            int v=it[1];
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+        queue<int>q;
+        vector<int>topo;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0)
+            q.push(i);
+        }
+        while(!q.empty()){
+            int node=q.front();
+            topo.push_back(node);
+            q.pop();
+            for(auto &pr:adj[node]){
+                indegree[pr]--;
+                if(indegree[pr]==0)
+                q.push(pr);
+            }
+        }
+        if(topo.size()!=V)
+        return {};
+        return topo;
+        // if(topo.size()==V)
+        // return true;
+        // return false;
+    }
+};
