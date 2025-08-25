a832ca51871c1efa39589518fc0dc591f8e0b362
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-08-26 01:10:48 +0530

    Time: 0 ms (100%), Space: 53 MB (73.04%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
+        vector<int> ans;
     bool dfs(int node, vector<vector<int>>& graph, vector<int>& vis,
-             vector<int>& pv, vector<int>& check) {
+             vector<int>& pv) {
         vis[node] = 1;
         pv[node] = 1;
-        check[node] = 0;
+        // check[node] = 0;
         for (int i : graph[node]) {
             if (!vis[i]) {
 
-                if (dfs(i, graph, vis, pv, check) == true) {
-                    check[node] = 0;
+                if (dfs(i, graph, vis, pv) == true) {
+                    // check[node] = 0;
                     return true;
                 }
             } else if (pv[i] == 1) {
-                check[node] = 0;
+                // check[node] = 0;
                 return true;
             }
         }
-        check[node] = 1;
+        // check[node] = 1;
+        ans.push_back(node);
         pv[node] = 0;
         return false;
     }
@@ -25,17 +27,18 @@ public:
         int V = graph.size();
         vector<int> vis(V,0);
         vector<int> pv(V,0);
-        vector<int> check(V,0);
+        // vector<int> check(V,0);
         for (int i = 0; i < V; i++) {
             if (!vis[i]) {
-                dfs(i, graph, vis, pv, check);
+                dfs(i, graph, vis, pv);
             }
         }
-        vector<int> ans;
-        for (int i = 0; i < V; i++) {
-            if (check[i] == 1)
-                ans.push_back(i);
-        }
+
+        // for (int i = 0; i < V; i++) {
+        //     if (check[i] == 1)
+        //         ans.push_back(i);
+        // }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
