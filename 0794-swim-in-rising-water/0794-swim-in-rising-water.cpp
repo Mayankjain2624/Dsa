577a7f9fce33b1135ff91e522d9278d9cab3f52d
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-08-27 16:19:42 +0530

    Time: 5 ms (70.41%), Space: 12.4 MB (98.59%) - LeetHub

---
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+ vector<int> dx = {1, -1, 0, 0};
+    vector<int> dy = {0, 0, -1, 1};
+    bool isvalid(int x, int y, int n, int m)
+    {
+        if (x >= 0 && x < n && y >= 0 && y < m)
+            return true;
+        return false;
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<vector<int>>ans(n,vector<int>(n,1e9));
+          priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+          pq.push({0,0});
+          ans[0][0]=grid[0][0];
+          while(!pq.empty()){
+            // int cost=pq.top().first;
+            // int node=pq.top().second;
+            int x=pq.top().first;
+            int y=pq.top().second;
+            pq.pop();
+            for (int di = 0; di < 4; di++)
+            {
+                int nx = x + dx[di];
+                int ny = y + dy[di];
+                // int newnode=nx*n+ny;
+                if (isvalid(nx, ny, n, n))
+                {
+                    if(ans[nx][ny]>max(ans[x][y],grid[nx][ny]))
+                    {
+                        ans[nx][ny]=max(ans[x][y],grid[nx][ny]);
+                        pq.push({nx,ny});
+                    }
+                }
+            }
+          }
+          return ans[n-1][n-1];
+    }
+};
