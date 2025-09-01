852b35be4ebe95c6a0c4bdf6bc63d0105923d88f
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-09-02 01:34:15 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.41%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -18,7 +18,14 @@ int maxloot(vector<int>&nums,int i,vector<int>&dp){
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int>dp(n,-1);
-        int ans=maxloot(nums,n-1,dp);
-        return ans;
+        // int ans=maxloot(nums,n-1,dp);
+        dp[0]=nums[0];
+        if(n>1)
+        dp[1]=max(dp[0],nums[1]);
+        for(int i=2;i<n;i++){
+               dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
+
     }
 };
