7131e73dc607716e98e5ee2baced5ec773c0aa2a
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-09-02 01:30:00 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.59%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int maxloot(vector<int>&nums,int i,vector<int>&dp){
+    if(i<0)
+    return 0;
+    if(i==0)
+    return nums[i];
+    if(dp[i]!=-1)
+    return dp[i];
+    else
+    {
+        int take=0,nottake=0;
+        take=nums[i]+maxloot(nums,i-2,dp);
+        nottake=maxloot(nums,i-1,dp);
+        return dp[i]=max(take,nottake);
+    }
+}
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        int ans=maxloot(nums,n-1,dp);
+        return ans;
+    }
+};
