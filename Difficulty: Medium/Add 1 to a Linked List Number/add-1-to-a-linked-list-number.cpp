85f469c0ab1f1e7d140592d90bfdbfbc0bffacdf
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-09-12 01:10:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -26,62 +26,28 @@ class Solution {
         return newhead;
     }
     Node* addOne(Node* head) {
-        // Node* newhead=reverseList(head);
-        // int s=newhead->data+1;
-        // newhead->data=s%10;
-        // s=s/10;
-        // if(newhead->next!=nullptr)
-        // {
-        // Node* temp=newhead->next;
-        // while(temp){
-        //     s=s+temp->data;
-        //     temp->data=s%10;
-        //     s=s/10;
-        //     temp=temp->next;
-        // }
-        // if(s)
-        // temp->next=new Node(s);
-        // newhead=reverseList(newhead);
-        // while(newhead->data==0)
-        // newhead=newhead->next;
-        // return newhead;
-        // }
-        // newhead->next=new Node(s);
-        //  newhead=reverseList(newhead);
-        //  while(newhead->data==0)
-        // newhead=newhead->next;
-        // return newhead;
-        
-        Node* newhead = reverseList(head);
-        int carry = 0;
-        Node* temp = newhead;
-        if(temp->data == 9)
-        {
-            carry = 1;
-            temp->data = 0;
-        }
-        else
-        {
-            temp->data = temp->data + 1;
-        }
-        while(temp->next != NULL && carry == 1)
-        {
-            temp = temp->next;
-            if(temp->data == 9)
-            {
-                temp->data = 0;
-            }
-            else
-            {
-                temp->data = temp->data + 1;
-                carry=0;
-            }
-        }
-        if(carry == 1)
-        {
-            Node* newnode = new Node(1);
-            temp->next = newnode;
-        }
-        return reverseList(newhead);
+       Node* newhead=reverseList(head);
+       int carry=0;
+       Node* temp=newhead;
+       Node* prev=nullptr;
+       while(temp){
+           if(temp->data==9){
+               temp->data=0;
+               carry=1;
+               prev=temp;
+               temp=temp->next;
+           }
+           else
+           {
+               temp->data=temp->data+1;
+               carry=0;
+               break;
+           }
+       }
+       if(carry){
+           prev->next=new Node(1);
+       }
+       head=reverseList(newhead);
+       return head;
     }
 };
