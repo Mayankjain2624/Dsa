fe4b2e0ed010cbc66b0b94866d3a0e238f2b242f
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-09-19 18:57:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -6,19 +6,20 @@ class Solution {
         // int m=mat[0].size();
         if(n==1)
         return 0;
-        vector<int>outdegree(n+1),indegree(n+1,0);
         for(int i=0;i<n;i++){
+            int s=0;
             for(int j=0;j<n;j++){
-                if(mat[i][j]==1 && i!=j)
-                {
-                outdegree[i]++;
-                indegree[j]++;
-                }
+                s+=mat[i][j];
             }
-        }
-        for(int i=0;i<n;i++){
-            if(outdegree[i]==0 && indegree[i]==n-1)
-            return i;
+            if(s==1)
+            {
+                int t=0;
+                for(int k=0;k<n;k++){
+                    t+=mat[k][i];
+                }
+                if(t==n)
+                return i;
+            }
         }
         return -1;
     }
