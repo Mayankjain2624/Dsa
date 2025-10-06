e414db2efa0d22d89f2854ad5e6239e0311fd29a
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-10-06 14:32:34 +0530

    Time: 11 ms (40.2%), Space: 12.4 MB (98.93%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
- vector<int> dx = {1, -1, 0, 0};
+    vector<int> dx = {1, -1, 0, 0};
     vector<int> dy = {0, 0, -1, 1};
-    bool isvalid(int x, int y, int n, int m)
-    {
+    bool isvalid(int x, int y, int n, int m) {
         if (x >= 0 && x < n && y >= 0 && y < m)
             return true;
         return false;
     }
     int swimInWater(vector<vector<int>>& grid) {
-        int n=grid.size();
-        vector<vector<int>>ans(n,vector<int>(n,1e9));
-        //  
-        queue<pair<int,int>>pq;
-          pq.push({0,0});
-          ans[0][0]=grid[0][0];
-          while(!pq.empty()){
-            int x=pq.front().first;
-            int y=pq.front().second;
+        int n = grid.size();
+        vector<vector<int>> ans(n, vector<int>(n, 1e9));
+        priority_queue<pair<int, int>, vector<pair<int, int>>,
+                       greater<pair<int, int>>>
+            pq;
+        pq.push({0, 0});
+        ans[0][0] = grid[0][0];
+        while (!pq.empty()) {
+
+            int x = pq.top().first;
+            int y = pq.top().second;
             pq.pop();
-            for (int di = 0; di < 4; di++)
-            {
+            for (int di = 0; di < 4; di++) {
                 int nx = x + dx[di];
                 int ny = y + dy[di];
-                if (isvalid(nx, ny, n, n))
-                {
-                    if(ans[nx][ny]>max(ans[x][y],grid[nx][ny]))
-                    {
-                        ans[nx][ny]=max(ans[x][y],grid[nx][ny]);
-                        pq.push({nx,ny});
+                if (isvalid(nx, ny, n, n)) {
+                    if (ans[nx][ny] > max(ans[x][y], grid[nx][ny])) {
+                        ans[nx][ny] = max(ans[x][y], grid[nx][ny]);
+                        pq.push({nx, ny});
                     }
                 }
             }
-          }
-          return ans[n-1][n-1];
+        }
+        return ans[n - 1][n - 1];
     }
 };
