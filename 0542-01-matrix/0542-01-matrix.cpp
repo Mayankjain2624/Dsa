b95984c9440dabb528ce07e7e616a162a3aa40b5
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-10-11 00:48:00 +0530

    Time: 21 ms (49.53%), Space: 38.9 MB (22.49%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0)
+                {
+                    vis[i][j]=1;
+                q.push({{i,j},0});
+                }
+            }
+        }
+        int ad=0;
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int dis=q.front().second;
+            ans[r][c]=dis;
+            q.pop();
+            for(int di=-1;di<=1;di++){
+                for(int dj=-1;dj<=1;dj++){
+                    if(di==0 || dj==0){
+                        int nr=r+di;
+                        int nc=c+dj;
+                        if(nr>=0 &&nr<n && nc>=0 &&nc<m){
+                            if(!vis[nr][nc]){
+                                vis[nr][nc]=1;
+                                q.push({{nr,nc},dis+1});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
