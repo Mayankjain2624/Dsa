0c7ee0085efcdbda3a71ff41a13cb66c8b35c52a
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-11-03 17:00:07 +0530

    Time: 1 ms (80.65%), Space: 110.7 MB (6.83%) - LeetHub

--- 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.cpp
@@ -1,41 +1,49 @@
 class Solution {
 public:
-    // long long int dp(int i,char prev,string &colors,vector<int>&needed){
-    //     if(i==0)
-    //     {
-    //         if(colors[i]==prev)
-    //         return needed[0];
-    //         else
-    //         return 0;
-    //     }
-    //     if(colors[i]==prev)
-    //     {
-    //         return
-    //         min(needed[i]+dp(i-1,prev,colors,needed),needed[i+1]+dp(i-1,colors[i],colors,needed));
-    //     }
-    //     else
-    //     return dp(i-1,colors[i],colors,needed);
-    // }
+    long long int dp(int i,int prev,string &colors,vector<int>&needed,vector<int>&dpv){
+        if(i<0)
+        {
+            return 0;
+            // if(colors[i]==colors[prev])
+            // return needed[0];
+            // else
+            // return 0;
+        }
+        if(dpv[i]!=-1)
+        return dpv[i];
+        int x=0;
+        if(colors[i]==colors[prev])
+        {
+            if(needed[i]>=needed[prev])
+            x= needed[prev]+dp(i-1,i,colors,needed,dpv);
+            else
+            x= needed[i]+dp(i-1,prev,colors,needed,dpv);
+        }
+        else
+        x=dp(i-1,i,colors,needed,dpv);
+        return dpv[i]=x;
+    }
     int minCost(string colors, vector<int>& neededTime) {
         long long int n = colors.size();
-        //    return dp(n-1,'#',colors,neededTime);
-        long long int maxi=neededTime[0];
-        long long int currSum=0;
-        long long int totalSum=neededTime[0];
-        for (long long int i = 1; i < n; i++) {
-            totalSum+=neededTime[i];
-              if(colors[i]==colors[i-1])
-              {
-                  maxi=max(maxi,neededTime[i]*1LL);
-              }
-              else
-              {
-                currSum+=maxi;
-                maxi=neededTime[i];
-              }
-        }
-        currSum+=maxi;
-        // cout<<totalSum<<" "<<currSum<<endl;
-        return totalSum-currSum;
+        vector<int>dpv(n,-1);
+           return dp(n-2,n-1,colors,neededTime,dpv);
+        // long long int maxi=neededTime[0];
+        // long long int currSum=0;
+        // long long int totalSum=neededTime[0];
+        // for (long long int i = 1; i < n; i++) {
+        //     totalSum+=neededTime[i];
+        //       if(colors[i]==colors[i-1])
+        //       {
+        //           maxi=max(maxi,neededTime[i]*1LL);
+        //       }
+        //       else
+        //       {
+        //         currSum+=maxi;
+        //         maxi=neededTime[i];
+        //       }
+        // }
+        // currSum+=maxi;
+        // // cout<<totalSum<<" "<<currSum<<endl;
+        // return totalSum-currSum;
     }
 };
