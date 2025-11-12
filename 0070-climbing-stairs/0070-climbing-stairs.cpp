6a8c27e2ca4c5cbdf98f950a43f3face53c06531
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-11-12 18:26:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (34.45%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-        int dp[46];
-    
-    // Solution() {
-    //     std::fill(dp, dp + 46, -1); 
-    // }
-
-//     int climbStairs(int n) {
-//          if(n<0)
-//          return 0;
-//          else if(n==0)
-//          return 1;
-//          else if(dp[n]!=-1)
-//          return dp[n];
-//          else
-//          return dp[n]=climbStairs(n-1)+climbStairs(n-2);
-//     }
-// };
-int climbStairs(int n) {
-    dp[0]=1;
-    dp[1]=1;
-    for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
+int dp(int i,int n,vector<int>&dpv)
+{
+    if(i==n)
+    return 1;
+    if(i>n)
+    return 0;
+    if(dpv[i]!=-1)
+    return dpv[i];
+    return dpv[i]=dp(i+1,n,dpv)+dp(i+2,n,dpv);
+}
+    int climbStairs(int n) {
+        vector<int>dpv(n+1,-1);
+        return dp(0,n,dpv);
     }
-    return dp[n];
-}};
-
+};
