6f863bd29f4eed2054b658a38b4c091d8e0ff84d
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-12-19 00:34:12 +0530

    Time: 19 ms (36.55%), Space: 328.3 MB (22.98%) - LeetHub

---
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
+        long long  n = prices.size();
+        vector<long long > prefix(n), ws(n);
+        // long long  sum = 0;
+        prefix[0] = prices[0];
+        for (long long  i = 1; i < n; i++) {
+            prefix[i] = prices[i] + prefix[i - 1];
+        }
+        ws[0] = prices[0] * strategy[0];
+        for (long long  i = 1; i < n; i++) {
+            ws[i] = ws[i - 1] + (prices[i] * strategy[i]);
+        }
+        // for(long long  i:prefix)
+        // cout<<i<<" ";
+        // cout<<endl;
+        // for(long long  i:ws)
+        // cout<<i<<" ";
+        // cout<<endl;
+        cout<<ws[n-1]<<endl;
+        long long  ans = max(prefix[k-1]-prefix[k/2-1]+ws[n-1]-ws[k-1],ws[n-1]);
+        // cout<<ans<<endl;
+        for (long long  i = 1; i < (n - k + 1); i++) {
+            long long  half = i + k / 2;
+            long long  last = i + k-1;
+            long long  sum = prefix[last] - prefix[half - 1] + ws[i - 1] + ws[n - 1] -
+                      ws[last];
+            ans = max(ans, sum);
+            // cout<<sum<<endl;
+        }
+        return ans;
+    }
+};
