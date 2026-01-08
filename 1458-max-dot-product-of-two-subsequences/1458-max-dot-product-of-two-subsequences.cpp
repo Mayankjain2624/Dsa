da3e546f603f50513d196e978ee2de201733d6a0
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-01-08 19:12:10 +0530

    Time: 14 ms (78%), Space: 17.9 MB (41.6%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-
-    int solve(vector<int>& nums1, vector<int>& nums2,int i,int j,vector<vector<int>>&dp){
-        if(i>=nums1.size())
-        return 0;
-        if(j>=nums2.size())
-        return 0;
-        if(dp[i][j]!=INT_MIN)
-        return dp[i][j];
-        int op1=nums1[i]*nums2[j]+solve(nums1,nums2,i+1,j+1,dp);
-        int op2=INT_MIN;
-        if(j+1<nums2.size())
-        op2=solve(nums1,nums2,i,j+1,dp);
-        int op3=INT_MIN;
-        if(i+1<nums1.size())
-        op3=solve(nums1,nums2,i+1,j,dp);
-        int op4=nums1[i]*nums2[j];
-        return dp[i][j]=max({op1,op2,op3,op4});
-    }
+    // int solve(vector<int>& nums1, vector<int>& nums2, int i, int j,
+    //           vector<vector<int>>& dp) {
+    //     if (i >= nums1.size() || j >= nums2.size())
+    //         return INT_MIN;
+    //     if (dp[i][j] != INT_MIN)
+    //         return dp[i][j];
+    //     int op1 =
+    //         nums1[i] * nums2[j] + max(0, solve(nums1, nums2, i + 1, j + 1, dp));
+    //     int op2 = solve(nums1, nums2, i, j + 1, dp);
+    //     int op3 = solve(nums1, nums2, i + 1, j, dp);
+    //     int op4 = nums1[i] * nums2[j];
+    //     return dp[i][j] = max({op1, op2, op3, op4});
+    // }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        vector<vector<int>>dp(n,vector<int>(m,INT_MIN));
-        return solve(nums1,nums2,0,0,dp);
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MIN));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int op1 = nums1[i] * nums2[j] + max(0, dp[i + 1][j + 1]);
+                int op2 = dp[i][j + 1];
+                int op3 = dp[i + 1][j];
+                int op4 = nums1[i] * nums2[j];
+                dp[i][j]= max({op1, op2, op3, op4});
+            }
+        }
+        return dp[0][0];
     }
-
 };
