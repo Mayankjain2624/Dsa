3f382821973d4dd020582c45253a85a638970a65
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-01-31 16:49:11 +0530

    Time: 0 ms (100%), Space: 15.6 MB (91.87%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,24 +3,21 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n=nums.size();
         // int mini=n-1;
+        int k=0;
         for(int i=n-1;i>=1;i--){
             if(nums[i-1]<nums[i]){
-              int minelement=nums[i],minelementindex=i;
-              for(int j=i;j<n;j++){
-                if(nums[j]>nums[i-1]){
-                     if(nums[j]<minelement)
-                     {
-                        minelement=nums[j];
-                        minelementindex=j;
-                     }
-                }
+              for(int j=n-1;j>=i;j--){
+                    if(nums[j]>nums[i-1])
+                    {
+                    swap(nums[i-1],nums[j]);
+                    break;
+                    }
               }
-              swap(nums[i-1],nums[minelementindex]);
-            sort(nums.begin()+i,nums.end());
-            return;
+            k=i;
+            break;
             }
         }
-        sort(nums.begin(),nums.end());
+       reverse(nums.begin() + k, nums.end());
         return;
     }
 };
