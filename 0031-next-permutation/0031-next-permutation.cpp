a7646f77315a8e3c41781f46181b70a14f853b98
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-01-31 16:36:00 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.18%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int l = -1, r = -1;
-        int len = nums.size();
-        for (int i = 1; i < len; i++) {
-            if (nums[i] > nums[i - 1])
-                r = i - 1;
-        }
-        if (r != -1) {
-            int k = -1;
-            for (int i = r + 1; i < len; i++) {
-                if (nums[i] > nums[r])
-                    k = i;
+        int n=nums.size();
+        // int mini=n-1;
+        for(int i=n-1;i>=1;i--){
+            if(nums[i-1]<nums[i]){
+              int minelement=nums[i],minelementindex=i;
+              for(int j=i;j<n;j++){
+                if(nums[j]>nums[i-1]){
+                     if(nums[j]<minelement)
+                     {
+                        minelement=nums[j];
+                        minelementindex=j;
+                     }
+                }
+              }
+              swap(nums[i-1],nums[minelementindex]);
+            sort(nums.begin()+i,nums.end());
+            return;
             }
-            swap(nums[r], nums[k]);
         }
-        reverse(nums.begin() + r + 1, nums.end());
+        sort(nums.begin(),nums.end());
+        return;
     }
 };
