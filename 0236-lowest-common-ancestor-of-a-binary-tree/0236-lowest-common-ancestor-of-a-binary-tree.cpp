e4ee417806d948af500837a56d5fd3e3f57f94e8
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-09 00:48:45 +0530

    Time: 10 ms (79.55%), Space: 17.4 MB (64.96%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,35 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
 class Solution {
 public:
-   vector<TreeNode*>pv,qv;
-  void solve(TreeNode* node, TreeNode* p, TreeNode* q,vector<TreeNode*>&temp){
-      if(node==nullptr)
-      return;
-      temp.push_back(node);
-      if(node==p)
-      pv=temp;
-      if(node==q)
-      qv=temp;
-      solve(node->left,p,q,temp);
-      solve(node->right,p,q,temp);
-      temp.pop_back();
-  }
+    vector<TreeNode*> pv, qv;
+    TreeNode* solve(TreeNode* node, TreeNode* p, TreeNode* q) {
+        if(node==p || node==q ||node==nullptr)
+        return node;
+        TreeNode* left=solve(node->left,p,q);
+        TreeNode* right=solve(node->right,p,q);
+        if(left==nullptr)
+        return right;
+        else if(right==nullptr)
+        return left;
+        else
+        return node;
+        
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-       vector<TreeNode*>temp;
-       solve(root,p,q,temp);
-        int i = 0;
-while (i < pv.size() && i < qv.size() && pv[i] == qv[i]) {
-    ++i;
-}
-return pv[i - 1];
-        return root;
+        return solve(root, p,q);
+        
     }
 };
