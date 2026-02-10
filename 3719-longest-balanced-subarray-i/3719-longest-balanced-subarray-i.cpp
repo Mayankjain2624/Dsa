5479f4f8d430071a5570772d699c8a6f4c5e0a0b
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-10 17:38:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        if(n==1)
+          return 1;
+    
+        set<int>st;
+        int l=0;
+        int r=0;
+        while(r<n){
+            
+        }
+    }
+};
