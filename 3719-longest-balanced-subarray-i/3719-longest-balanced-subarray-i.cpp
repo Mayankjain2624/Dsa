cda7315bd62794b92a6e0c85ddc2557c45ad8073
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-10 17:38:10 +0530

    Time: 2373 ms (6.72%), Space: 472.8 MB (76.31%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -7,11 +7,24 @@ public:
         if(n==1)
           return 1;
     
-        set<int>st;
-        int l=0;
-        int r=0;
-        while(r<n){
-            
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            int odd=0, even=0;
+            set<int> s;
+            for(int j=i;j<n;j++){
+                if(nums[j]%2!=0 && s.find(nums[j])==s.end()){
+                    s.insert(nums[j]);
+                    odd++;
+                }
+                else if(nums[j]%2==0 && s.find(nums[j])==s.end()){
+                    s.insert(nums[j]);
+                    even++;
+                }
+                
+                if(odd==even)
+                  maxi=max(maxi,j-i+1);
+            }
         }
+        return maxi;
     }
 };
