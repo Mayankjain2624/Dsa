65f8d92466c318001c9e7a8b4a9d7b730ac8428b
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-16 03:21:10 +0530

    Time: 0 ms (100%), Space: 8.7 MB (95.87%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    // Time: O(N)
+    // Space: O(N)
+    string addBinary(string a, string b) {
+        // store ans
+        string ans = "";
+        // carry
+        int c = 0;
+        // start from last to avoid reverse saves O[n]
+        int i = a.size() - 1, j = b.size() - 1;
+        // loop till any rem dig or c > 0
+        while (i >= 0 || j >= 0 || c > 0) {
+            // curr sum of dig
+            int s = c;
+            if (i >= 0) {
+                s += a[i]-'0';
+                i--;
+            }
+            if (j >= 0) {
+                s += b[j]-'0';
+                j--;
+            }
+            // update ans 
+            ans += (s % 2)+ '0';
+            // update carry
+            c = s / 2;
+        }
+        // return ans
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
