454a9a7f9b8b7bb952ed339b29c9cc30f1eaecea
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-21 22:56:09 +0530

    Time: 0 ms (100%), Space: 31.3 MB (42.5%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,30 +11,28 @@
  */
 class Solution {
 public:
-    int righth(TreeNode* node){
-        int h=0;
-        while(node!=nullptr)
-        {
-            h++;
-            node=node->left;
-        }
-        return h;
-    }
-    int lefth(TreeNode* node){
-        int h=0;
-        while(node!=nullptr)
-        {
-            h++;
-            node=node->right;
-        }
-        return h;
-    }
+    // int righth(TreeNode* node){
+    //     int h=0;
+    //     while(node!=nullptr)
+    //     {
+    //         h++;
+    //         node=node->left;
+    //     }
+    //     return h;
+    // }
+    // int lefth(TreeNode* node){
+    //     int h=0;
+    //     while(node!=nullptr)
+    //     {
+    //         h++;
+    //         node=node->right;
+    //     }
+    //     return h;
+    // }
     int countNodes(TreeNode* root) {
         int ans=0;
         if(root==nullptr)
         return 0;
-        int lh=lefth(root->left);
-        int rh=righth(root->right);
         return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
