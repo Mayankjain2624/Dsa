e49b6ffa113544304d3afc24a07f28e262c3b090
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-22 17:50:45 +0530

    Time: 27 ms (56.56%), Space: 17.7 MB (43.03%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,34 +1,79 @@
-class Solution {
+class Dsu {
 public:
-bool isValid(int x,int y,int n,int m){
-    if(x>=0 && x<n && y>=0 && y<m)
-    return true;
-    return false;
-}
-    vector<int>di={1,0,-1,0};
-    vector<int>dj={0,1,0,-1};
-    void dfs(vector<vector<char>>& grid,int x,int y,int n,int m){
-        grid[x][y]='0';
-        for(int i=0;i<4;i++){
-            int nx=x+di[i];
-            int ny=y+dj[i];
-            if(isValid(nx,ny,n,m) && grid[nx][ny]=='1'){
-                dfs(grid,nx,ny,n,m);
-            }
+    vector<int> parent, size;
+    Dsu(int n) {
+        parent.resize(n);
+        size.resize(n);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
+    int find(int u) {
+        if (parent[u] == u) {
+            return u;
+        }
+           return parent[u] = find(parent[u]);
+    }
+    void unite(int u, int v) {
+        int ulp_u = find(u);
+        int ulp_v = find(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] > size[ulp_v]) {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        } else {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+    }
+};
+class Solution {
+public:
+    bool isValid(int x, int y, int n, int m) {
+        if (x >= 0 && x < n && y >= 0 && y < m)
+            return true;
+        return false;
+    }
+    vector<int> di = {1, 0, -1, 0};
+    vector<int> dj = {0, 1, 0, -1};
+    // void dfs(vector<vector<char>>& grid, int x, int y, int n, int m) {
+    //     grid[x][y] = '0';
+    //     for (int i = 0; i < 4; i++) {
+    //         int nx = x + di[i];
+    //         int ny = y + dj[i];
+    //         if (isValid(nx, ny, n, m) && grid[nx][ny] == '1') {
+    //             dfs(grid, nx, ny, n, m);
+    //         }
+    //     }
+    // }
+    int index(int i, int j, int m) { return i * m + j; }
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    cnt++;
-                    dfs(grid,i,j,n,m);
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt = 0;
+        int s = n * m + 1;
+        Dsu dsu(s);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    for (int d = 0; d < 4; d++) {
+                        int nx = i + di[d];
+                        int ny = j + dj[d];
+                        if (isValid(nx, ny, n, m) && grid[nx][ny] == '1') {
+                            dsu.unite(index(i, j, m), index(nx, ny, m));
+                        }
+                    }
                 }
             }
         }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    int ti = index(i, j, m);
+                    if (dsu.parent[ti] == ti && grid[i][j] == '1')
+                        cnt++;
+                }
+            }
         return cnt;
     }
 };
