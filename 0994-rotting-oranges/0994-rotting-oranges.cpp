60d5264108f63c3d997a717d73c415669afffad1
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-22 14:21:23 +0530

    Time: 0 ms (100%), Space: 16.8 MB (56.17%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -10,12 +10,12 @@ bool isValid(int x,int y,int n,int m){
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-        queue<pair<int,pair<int,int>>>q; //i,j,time
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<int,pair<int,int>>>q;
+        // vector<vector<int>>vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    vis[i][j]=1;
+                    // vis[i][j]=1;
                     q.push({0,{i,j}});
                 }
             }
@@ -30,15 +30,16 @@ bool isValid(int x,int y,int n,int m){
             for(int i=0;i<4;i++){
                 int nx=x+di[i];
                 int ny=y+dj[i];
-                if(isValid(nx,ny,n,m) && !vis[nx][ny] && grid[nx][ny]==1){
-                      vis[nx][ny]=1;
+                if(isValid(nx,ny,n,m) && grid[nx][ny]==1){
+                    //   vis[nx][ny]=1;
+                      grid[nx][ny]=2;
                       q.push({time+1,{nx,ny}});
                 }
             }
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]==1)
+                if(grid[i][j]==1)
                 return -1;
             }
         }
