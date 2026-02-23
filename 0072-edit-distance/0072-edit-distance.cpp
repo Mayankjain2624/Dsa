d97fe81fdd1bd7f1ebbe2a29833d480eaf47d906
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-24 00:41:27 +0530

    Time: 9 ms (40.26%), Space: 13.2 MB (42.36%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -26,17 +26,17 @@ public:
         int n=word1.size();
         int m=word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(word1,word2,n-1,m-1,dp);
+        // return solve(word1,word2,n-1,m-1,dp);
         if(n==0)
         return m;
         if(m==0)
         return n;
-        for(int i=1;i<=n;i++){
-            dp[i][0]=i+1;
-        }
-        for(int j=1;j<=m;j++){
-            dp[0][j]=j+1;
-        }
+        for(int i=0;i<=n;i++){
+    dp[i][0]=i;
+}
+for(int j=0;j<=m;j++){
+    dp[0][j]=j;
+}
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(word1[i-1]==word2[j-1]){
