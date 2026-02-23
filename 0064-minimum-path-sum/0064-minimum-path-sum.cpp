8710059a06fe2788dd29c2b9618a655cc0aa1ff0
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-23 20:30:57 +0530

    Time: 0 ms (100%), Space: 16.3 MB (60.24%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,55 +1,25 @@
-//dijstra solution(my first intitution)
-
-// class Solution {
-// public:
-//     int minPathSum(vector<vector<int>>& grid) {
-//         int m=grid.size();
-//         int n=grid[0].size();
-//         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>>pq;
-//         pq.push({grid[0][0],{0,0}});
-//         int ans=0;
-//         if(m==1 && n==1)
-//         return grid[0][0];
-//         vector<vector<int>>cost(m,vector<int>(n,1e9));
-//         while(!pq.empty()){
-//             int c=pq.top().first;
-//             int x=pq.top().second.first;
-//             int y=pq.top().second.second;
-//             ans+=c;
-//             pq.pop();
-//             if(x<m-1 &&  cost[x+1][y]>c+grid[x+1][y])
-//             {
-//             cost[x+1][y]=c+grid[x+1][y];
-//             pq.push({cost[x+1][y],{x+1,y}});
-//             }
-//             if(y<n-1 &&cost[x][y+1]>c+grid[x][y+1])
-//             {
-//                 cost[x][y+1]=c+grid[x][y+1];
-//                 pq.push({cost[x][y+1],{x,y+1}});
-//             }
-//         }
-//         return cost[m-1][n-1];
-//     }
-// };
-
-
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid,int i,int j,vector<vector<int>>& dp){
-        if(i==0 && j==0)
-        return grid[0][0];
-        if(i<0 || j<0)
-        return 1000000;
-        if(dp[i][j]!=-1)
-        return dp[i][j];
-        int left=solve(grid,i,j-1,dp)+grid[i][j];
-        int up=solve(grid,i-1,j,dp)+grid[i][j];
-        return dp[i][j]=min(left,up);
-    }
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(grid,m-1,n-1,dp);
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=INT_MAX;
+        }
+        for(int j=0;j<=m;j++){
+            dp[0][j]=INT_MAX;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1 && j==1)
+                {
+                    dp[i][j]=grid[i-1][j-1];
+                    continue;
+                }
+                dp[i][j]=grid[i-1][j-1]+min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
     }
 };
