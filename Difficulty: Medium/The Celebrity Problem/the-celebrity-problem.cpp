7af3f742091d6afaf068f40f2faad045e79ef4aa
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2026-02-28 02:43:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -2,41 +2,43 @@ class Solution {
   public:
     int celebrity(vector<vector<int>>& mat) {
         // code here
-        int l=0;
-        int r=mat.size()-1;
+        int n=mat.size();
+        int l=0,r=n-1;
         while(l<=r){
+            // cout<<l<<r<<endl;
             if(l==r)
             {
-                for(int i=0;i<mat.size();i++){
-                    if(mat[i][l]==0)
+                for(int i=0;i<n;i++){
+                    if(mat[i][r]==0)
                     return -1;
                 }
-                for(int i=0;i<mat.size();i++){
-                    if(l==i)
-                    continue;
-                    if(mat[l][i]==1)
+                for(int j=0;j<n;j++)
+                {
+                    if(mat[l][j]==1 && j!=l)
                     return -1;
                 }
-                return l;
+            return l;
             }
-            if(mat[l][r]==1 && mat[r][l]==1)
-            {
+            if(mat[l][r]==mat[r][l]){
                 l++;
                 r--;
+                // cout<<mat[l][r]<<" "<<mat[r][l]<<endl;
+                // // cout<<mat[0][2]<<endl;
             }
-            else if(mat[l][r]==0 && mat[r][l]==0){
-                l++;
-                r--;
-            }
-            else if(mat[l][r]==1)
-            {
+            else if(mat[l][r]==1){
                 l++;
             }
-            else
-            {
-                r--;
-            }
+            else if(mat[r][l]==1)
+            r--;
         }
         return -1;
     }
 };
+
+
+
+
+
+
+
+
